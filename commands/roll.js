6a01638dd3dcec3f196944bba5ebc9b0c1cc2d8a
Michal Štefan <kibubek@gmail.com>
2024-07-15 00:06:39 +0200

    fixed reroll

--- commands/roll.js
+++ commands/roll.js
@@ -11,43 +11,42 @@ module.exports = {
                 .setRequired(true)),
 
     async execute(interaction) {
-        let amount = interaction.options.getInteger('castka');
-        let rollResult = Math.floor(Math.random() * amount) + 1;
+        const amount = interaction.options.getInteger('castka');
+        const rollResult = roll(amount);
 
         const row = new ActionRowBuilder()
             .addComponents(
                 new ButtonBuilder()
-                    .setCustomId('reroll')
+                    .setCustomId(`reroll_${rollResult}`)
                     .setLabel('Pokračovat v dalším rollu')
                     .setStyle(ButtonStyle.Primary)
             );
 
         await interaction.reply({
-            content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
+            content: `${interaction.user.username} rollnul ${rollResult} (1-${amount})`,
             components: [row]
         });
 
-        const filter = i => i.customId === 'reroll' && i.user.id === interaction.user.id;
+        const filter = i => i.customId.startsWith('reroll');
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
 
         collector.on('collect', async i => {
-            if (i.customId === 'reroll') {
-                amount = rollResult;
-                rollResult = Math.floor(Math.random() * amount) + 1;
+            await i.deferUpdate();
+            const newAmount = parseInt(i.customId.split('_')[1]);
+            const newRollResult = roll(newAmount);
 
-                const newRow = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('reroll')
-                            .setLabel('Pokračovat v dalším rollu')
-                            .setStyle(ButtonStyle.Primary)
-                    );
+            const newRow = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`reroll_${newRollResult}`)
+                        .setLabel('Pokračovat v dalším rollu')
+                        .setStyle(ButtonStyle.Primary)
+                );
 
-                await i.reply({
-                    content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
-                    components: [newRow]
-                });
-            }
+            await i.followUp({
+                content: `${i.user.username} rollnul ${newRollResult} (1-${newAmount})`,
+                components: [newRow]
+            });
         });
 
         collector.on('end', async collected => {
@@ -55,15 +54,19 @@ module.exports = {
                 .addComponents(
                     new ButtonBuilder()
                         .setCustomId('reroll')
-                        .setLabel('Re-roll')
+                        .setLabel('Pokračovat v dalším rollu')
                         .setStyle(ButtonStyle.Primary)
                         .setDisabled(true)
                 );
 
             await interaction.editReply({
-                content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
+                content: `${interaction.user.username} rollnul ${rollResult} (1-${amount})`,
                 components: [disabledRow]
             });
         });
+
+        function roll(max) {
+            return Math.floor(Math.random() * max) + 1;
+        }
     }
 };
