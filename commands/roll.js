788bacf01943282dfbb498bfc0e75222e6290549
Michal Štefan <kibubek@gmail.com>
2024-07-14 23:57:18 +0200

    added roll button

--- commands/roll.js
+++ commands/roll.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,10 +11,59 @@ module.exports = {
                 .setRequired(true)),
 
     async execute(interaction) {
-        const amount = interaction.options.getInteger('castka');
+        let amount = interaction.options.getInteger('castka');
+        let rollResult = Math.floor(Math.random() * amount) + 1;
 
-        const rollResult = Math.floor(Math.random() * amount) + 1;
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('reroll')
+                    .setLabel('Pokračovat v dalším rollu')
+                    .setStyle(ButtonStyle.Primary)
+            );
 
-        await interaction.reply(`${interaction.user.username} rollnul ${rollResult} (1-${amount})`);
+        await interaction.reply({
+            content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
+            components: [row]
+        });
+
+        const filter = i => i.customId === 'reroll' && i.user.id === interaction.user.id;
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
+
+        collector.on('collect', async i => {
+            if (i.customId === 'reroll') {
+                amount = rollResult;
+                rollResult = Math.floor(Math.random() * amount) + 1;
+
+                const newRow = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('reroll')
+                            .setLabel('Pokračovat v dalším rollu')
+                            .setStyle(ButtonStyle.Primary)
+                    );
+
+                await i.reply({
+                    content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
+                    components: [newRow]
+                });
+            }
+        });
+
+        collector.on('end', async collected => {
+            const disabledRow = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('reroll')
+                        .setLabel('Re-roll')
+                        .setStyle(ButtonStyle.Primary)
+                        .setDisabled(true)
+                );
+
+            await interaction.editReply({
+                content: `## ${interaction.user.username} rollnul **${rollResult}** (1-${amount})`,
+                components: [disabledRow]
+            });
+        });
     }
 };
