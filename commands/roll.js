791ed11d33e4055c619eccbf326398d9c1887ffa
Michal Štefan <kibubek@gmail.com>
2024-07-15 00:11:35 +0200

    reverted roll

--- commands/roll.js
+++ commands/roll.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -12,61 +11,9 @@ module.exports = {
 
     async execute(interaction) {
         const amount = interaction.options.getInteger('castka');
-        const rollResult = roll(amount);
 
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId(`reroll_${rollResult}`)
-                    .setLabel('Pokračovat v dalším rollu')
-                    .setStyle(ButtonStyle.Primary)
-            );
+        const rollResult = Math.floor(Math.random() * amount) + 1;
 
-        await interaction.reply({
-            content: `${interaction.user.username} rollnul ${rollResult} (1-${amount})`,
-            components: [row]
-        });
-
-        const filter = i => i.customId.startsWith('reroll');
-        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 });
-
-        collector.on('collect', async i => {
-            await i.deferUpdate();
-            const newAmount = parseInt(i.customId.split('_')[1]);
-            const newRollResult = roll(newAmount);
-
-            const newRow = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId(`reroll_${newRollResult}`)
-                        .setLabel('Pokračovat v dalším rollu')
-                        .setStyle(ButtonStyle.Primary)
-                );
-
-            await i.followUp({
-                content: `${i.user.username} rollnul ${newRollResult} (1-${newAmount})`,
-                components: [newRow]
-            });
-        });
-
-        collector.on('end', async collected => {
-            const disabledRow = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('reroll')
-                        .setLabel('Pokračovat v dalším rollu')
-                        .setStyle(ButtonStyle.Primary)
-                        .setDisabled(true)
-                );
-
-            await interaction.editReply({
-                content: `${interaction.user.username} rollnul ${rollResult} (1-${amount})`,
-                components: [disabledRow]
-            });
-        });
-
-        function roll(max) {
-            return Math.floor(Math.random() * max) + 1;
-        }
+        await interaction.reply(${ interaction.user.username } rollnul ${ rollResult }(1 - ${ amount }));
     }
 };
